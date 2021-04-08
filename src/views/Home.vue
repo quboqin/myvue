25757e911b8acf53f27adbcf223eeb22f0a0543a
Qubo Qin <qubo.qin.2018@gmail.com>
2021-04-08 22:31:48 +0800

    update home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App + CI + CD" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App + CI and CD" />
   </div>
 </template>
 
