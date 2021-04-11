8942f205064bf1ecf00a022f14d8cedeeaa2c1c5
Qubo Qin <qubo.qin.2018@gmail.com>
2021-04-11 11:49:43 +0800

    build in jenkins

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App + CI and CD" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App + Jenkins" />
   </div>
 </template>
 
