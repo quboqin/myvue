b22e263dd23f6394e22b553f8619ae807cebf81a
Qubo Qin <qubo.qin.2018@gmail.com>
2021-04-13 18:43:29 +0800

    test heroku

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld
-      msg="Welcome to Your Vue.js + TypeScript App + CI + CD + Heroku"
+      msg="Welcome to Your Vue.js + TypeScript App + CI + CD + Heroku + Github"
     />
     <div>
       {{ appName }}
